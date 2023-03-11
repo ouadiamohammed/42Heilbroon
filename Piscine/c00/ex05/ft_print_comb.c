88613fe508d19c8e33e7fc8cbcbc8c2a5198a860
mouadia <ouadia.mohammed@outlook.de>
2023-03-12 00:06:14 +0100

    ex05

---
+++ Piscine/c00/ex05/ft_print_comb.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+void	 ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while ( c <= '9')
+			{
+				write(1, &a, 1);
+				write(1, &b, 1);
+				write(1, &c, 1);
+				if (a < '7')
+					write(1, &", ", 2);
+				else
+					break;
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+int main (void)
+{
+	ft_print_comb();
+	write(1, &"\n", 1);
+}
