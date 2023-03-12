5e71f438abe21b5ba4265cd2379faf952d19713c
mouadia <ouadia.mohammed@outlook.de>
2023-03-12 15:22:35 +0100

    ex01

---
+++ Piscine/c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void ft_ultimate_ft(int *********nbr)
+{
+    *********nbr = 42;
+}
+
+int main()
+{
+    int a;
+    int *********nbr1;
+    int ********nbr2;
+    int *******nbr3;
+	int ******nbr4;
+	int *****nbr5;
+	int ****nbr6;
+	int ***nbr7;
+	int **nbr8;
+	int *nbr9;
+
+    nbr1 = &nbr2;
+    nbr2 = &nbr3;
+    nbr3 = &nbr4;
+	nbr4 = &nbr5;
+	nbr5 = &nbr6;
+	nbr6 = &nbr7;
+	nbr7 = &nbr8;
+	nbr8 = &nbr9;
+	nbr9 = &a;
+
+    ft_ultimate_ft(nbr1);
+    printf("%d", a);
+	return 0;
+}
