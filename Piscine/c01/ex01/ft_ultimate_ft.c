4c7659972203e858976a9c20b7384be7c979d779
Ouadia Mohammed <114778838+ouadiamohammed@users.noreply.github.com>
2023-03-13 19:21:00 +0100

    Update ft_ultimate_ft.c

--- Piscine/c01/ex01/ft_ultimate_ft.c
+++ Piscine/c01/ex01/ft_ultimate_ft.c
@@ -11,24 +11,24 @@ int main()
     int *********nbr1;
     int ********nbr2;
     int *******nbr3;
-	int ******nbr4;
-	int *****nbr5;
-	int ****nbr6;
-	int ***nbr7;
-	int **nbr8;
-	int *nbr9;
+    int ******nbr4;
+    int *****nbr5;
+    int ****nbr6;
+    int ***nbr7;
+    int **nbr8;
+    int *nbr9;
 
     nbr1 = &nbr2;
     nbr2 = &nbr3;
     nbr3 = &nbr4;
-	nbr4 = &nbr5;
-	nbr5 = &nbr6;
-	nbr6 = &nbr7;
-	nbr7 = &nbr8;
-	nbr8 = &nbr9;
-	nbr9 = &a;
+    nbr4 = &nbr5;
+    nbr5 = &nbr6;
+    nbr6 = &nbr7;
+    nbr7 = &nbr8;
+    nbr8 = &nbr9;
+    nbr9 = &a;
 
     ft_ultimate_ft(nbr1);
     printf("%d", a);
-	return 0;
+    return 0;
 }
